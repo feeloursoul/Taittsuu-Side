4a3fea9f281a33b78f32fffb70092c606a39fe10
feeloursoul <grow20kids@gmail.com>
2023-12-16 23:42:16 +0900

    first commit

---
+++ Taittsuu-side.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var popup = window.open('https://taittsuu.com/publictimeline', '_blank', 'width=450,height=900,top=0,right=0'); popup.moveTo(window.innerWidth - 300, 0);})();
